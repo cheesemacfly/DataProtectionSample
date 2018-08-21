ca41f5838db6e72dd9454839d9e5d031a88ac2d4
vivek kumar <vivek1273@gmail.com>
2018-08-21 22:43:02 +0530

    Initial Checkin

---
+++ WebApplication1/DataProtectionKeyRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace WebApplication1
+{
+    public class DataProtectionKeyRepository : IXmlRepository
+    {
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {            
+            return null;
+        }
+
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }            
+        }
+    }
+
+}
